81aa05ee614cac26a1b87e60cbf3062a9abd24da
koki <k0k1fish@outlook.com>
2025-09-16 02:02:45 +0800

    low-level mcp server

--- ArkTS/arkts_api.py
+++ ArkTS/arkts_api.py
@@ -1,15 +1,22 @@
+import asyncio
+import logging
 import os
+import pickle
+import re
 import subprocess
+import sys
+from logging.handlers import RotatingFileHandler
 from typing import Any, Dict, Iterable, List, Optional, Set, Tuple, Union
 from urllib.parse import quote, unquote
 
 import ohre
 import ohre.misc.utils as oh_utils
+from mcp.server.fastmcp.exceptions import ResourceError
 from ohre.abcre.dis.AsmMethod import AsmMethod
 from ohre.abcre.dis.DisFile import DisFile
 from ohre.abcre.dis.PandaReverser import PandaReverser
 from ohre.core import oh_app, oh_hap
-import pickle
+from pydantic import AnyUrl, BaseModel, Field, FileUrl
 
 ohre.set_log_print(False)
 ARK_DISASM = "tools/ark_disasm"
@@ -20,13 +27,29 @@ LOCAL_DEFAULT_PANDARE_PKL = os.path.join(VULMCP_ROOT_PATH, "main.pkl")
 panda_re_global: PandaReverser | None = None
 module_methd_name_l: List[str] | None = None
 
+VULMCP_ROOT_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s %(levelname)s %(message)s",
+    stream=sys.stderr
+)
+log_name = "ArkTS_API"
+log_file = os.path.join(VULMCP_ROOT_PATH, log_name + ".log")
+Log = logging.getLogger(log_name)
+handle = RotatingFileHandler(log_file, mode="a", maxBytes=50 * 1024 * 1024,
+                             backupCount=10, encoding="utf-8", delay=0)
+handle.setLevel(logging.INFO)
+handle.setFormatter(logging.Formatter("%(asctime)s %(levelname)s %(message)s"))
+Log.addHandler(handle)
+Log.setLevel(logging.DEBUG)
 
-def pickle_save_object(obj, filename: str):
+
+async def pickle_save_object(obj, filename: str):
     with open(filename, "wb") as file:
         pickle.dump(obj, file)
 
 
-def pickle_load_object(filename: str):
+async def pickle_load_object(filename: str):
     size = os.path.getsize(filename)
     if size == 0:
         return None
@@ -35,11 +58,11 @@ def pickle_load_object(filename: str):
     return obj
 
 
-def disasm(in_path: str = os.path.join(VULMCP_ROOT_PATH, "main.dis"), USE_LOCAL_PICKLE: bool = True):
+async def disasm(in_path: str = os.path.join(VULMCP_ROOT_PATH, "main.dis"), USE_LOCAL_PICKLE: bool = True):
     global panda_re_global
     try:
         if USE_LOCAL_PICKLE and os.path.isfile(LOCAL_DEFAULT_PANDARE_PKL):
-            panda_re: PandaReverser = pickle_load_object(LOCAL_DEFAULT_PANDARE_PKL)
+            panda_re: PandaReverser = await pickle_load_object(LOCAL_DEFAULT_PANDARE_PKL)
             if isinstance(panda_re, PandaReverser):
                 panda_re_global = panda_re
                 return
@@ -58,14 +81,14 @@ def disasm(in_path: str = os.path.join(VULMCP_ROOT_PATH, "main.dis"), USE_LOCAL_
     panda_re: PandaReverser = PandaReverser(dis_file)
     panda_re.trans_lift_all_method(DEBUG_LV=0)
     panda_re.module_analysis_algorithms()
-    pickle_save_object(panda_re, LOCAL_DEFAULT_PANDARE_PKL)
+    await pickle_save_object(panda_re, LOCAL_DEFAULT_PANDARE_PKL)
     panda_re_global = panda_re
 
 
-def get_all_module_method() -> List[str]:
+async def get_all_module_method() -> List[str]:
     global panda_re_global, module_methd_name_l
     if panda_re_global is None:
-        disasm()
+        await disasm()
     panda_re = panda_re_global
     if panda_re is None:
         raise ValueError("panda_re_global is None, please run disasm first")
@@ -96,6 +119,40 @@ async def get_external_file_content(file_name: str) -> str:
     global panda_re_global
     raise NotImplementedError()
 
+
+async def read_pa_by_url(uri: AnyUrl) -> list[str]:
+    """get ArkTS assembly by uri like panda://module.method, wildcard matching supported."""
+    Log.info(f"read-pa: uri: {uri} | host:{uri.host} path:{uri.path} {uri.port} {uri.query} {uri.unicode_string()}")
+    VALUE_ERROR_MSG = f"Invalid resource path: {uri}. Valid resource URL example: panda://Index%26.%23%2A%23 (for specific match of Index&.#*#), panda://*module_name* (for wildcard matching of all methods in module/class named `module_name`). Check URL encoding or use wildcard matching if it still failed."
+    res_pattern = uri.unicode_string()
+    if len(res_pattern) <= 8 or not res_pattern.startswith("panda://"):
+        raise ResourceError(VALUE_ERROR_MSG)
+    res_pattern = res_pattern.lstrip("panda://")
+    all_res = await get_all_module_method()  # original module.method name, NOT quoted
+
+    matched_res = []
+    if unquote(res_pattern) in all_res:  # one-to-one match
+        matched_res = [res_pattern]
+    elif res_pattern in all_res:  # one-to-one match
+        matched_res = [quote(res_pattern)]
+    elif "*" in res_pattern:  # * match mode
+        pattern = res_pattern.replace(".", r"\.").replace("*", ".*")
+        pattern_unquoted = unquote(res_pattern).replace(".", r"\.").replace("*", ".*")
+        regex = re.compile(f"^{pattern}$")  # re.compile(f"^{pattern}$")
+        regex_unquoted = re.compile(f"^{pattern_unquoted}$")
+        matched_res = [quote(res) for res in all_res if (regex.match(quote(res)) or regex.match(res))]
+        matched_res.extend([quote(res) for res in all_res if (
+            regex_unquoted.match(quote(res)) or regex_unquoted.match(res))])
+
+    if len(matched_res) == 0:  # It is better to return more unnecessary code than to return nothing
+        matched_res = [quote(s) for s in all_res if (unquote(res_pattern) in s or res_pattern in s)]
+
+    tasks = [get_module_method_panda_assembly_code(unquote(resource)) for resource in matched_res]
+    contents: list[str] = await asyncio.gather(*tasks)
+    if len(contents) == 0:
+        raise ResourceError(
+            VALUE_ERROR_MSG + f" Matched resource URL not exists. matched_res: {matched_res}. contents {contents}. res_pattern {res_pattern}")
+    return contents
 
 if __name__ == "__main__":
     disasm()
--- ArkTS/mcp_server.py
+++ ArkTS/mcp_server.py
@@ -10,7 +10,7 @@ from urllib.parse import quote, unquote
 import arkts_api
 import mcp.types as types
 from mcp.server import Server
-from mcp.server.fastmcp.exceptions import ResourceError
+
 from mcp.shared.context import RequestContext
 from mcp.server.fastmcp.server import Context
 from mcp.server.lowlevel import NotificationOptions, Server
@@ -50,40 +50,6 @@ class PandaAssemblyModuleMethodName(BaseModel):
         description="The module and method name of the Panda Assembly format file. e.g. A.B means module A method B",)
 
 
-async def read_pa_by_url(uri: AnyUrl) -> list[str]:
-    Log.info(f"read-pa: uri: {uri} | host:{uri.host} path:{uri.path} {uri.port} {uri.query} {uri.unicode_string()}")
-    VALUE_ERROR_MSG = f"Invalid resource path: {uri}. Valid resource URL example: panda://Index%26.%23%2A%23 (for specific match of Index&.#*#), panda://*module_name* (for wildcard matching of all methods in module/class named `module_name`). Check URL encoding or use wildcard matching if it still failed."
-    res_pattern = uri.unicode_string()
-    if len(res_pattern) <= 8 or not res_pattern.startswith("panda://"):
-        raise ResourceError(VALUE_ERROR_MSG)
-    res_pattern = res_pattern.lstrip("panda://")
-    all_res = arkts_api.get_all_module_method()  # original module.method name, NOT quoted
-
-    matched_res = []
-    if unquote(res_pattern) in all_res:  # one-to-one match
-        matched_res = [res_pattern]
-    elif res_pattern in all_res:  # one-to-one match
-        matched_res = [quote(res_pattern)]
-    elif "*" in res_pattern:  # * match mode
-        pattern = res_pattern.replace(".", r"\.").replace("*", ".*")
-        pattern_unquoted = unquote(res_pattern).replace(".", r"\.").replace("*", ".*")
-        regex = re.compile(f"^{pattern}$")  # re.compile(f"^{pattern}$")
-        regex_unquoted = re.compile(f"^{pattern_unquoted}$")
-        matched_res = [quote(res) for res in all_res if (regex.match(quote(res)) or regex.match(res))]
-        matched_res.extend([quote(res) for res in all_res if (
-            regex_unquoted.match(quote(res)) or regex_unquoted.match(res))])
-
-    if len(matched_res) == 0:  # It is better to return more unnecessary code than to return nothing
-        matched_res = [quote(s) for s in all_res if (unquote(res_pattern) in s or res_pattern in s)]
-
-    tasks = [arkts_api.get_module_method_panda_assembly_code(unquote(resource)) for resource in matched_res]
-    contents: list[str] = await asyncio.gather(*tasks)
-    if len(contents) == 0:
-        raise ResourceError(
-            VALUE_ERROR_MSG + f" Matched resource URL not exists. matched_res: {matched_res}. contents {contents}. res_pattern {res_pattern}")
-    return contents
-
-
 async def get_resource_related(code_or_name: str) -> list[TextContent]:
     Log.info(f"get_resource_related: read: {code_or_name}")
     contents: list = []
@@ -91,7 +57,7 @@ async def get_resource_related(code_or_name: str) -> list[TextContent]:
         if not code_or_name.startswith("panda://"):
             code_or_name = "panda://" + code_or_name
 
-        contents = await read_pa_by_url(AnyUrl(code_or_name))
+        contents = await arkts_api.read_pa_by_url(AnyUrl(code_or_name))
 
         if len(contents):
             return [TextContent(type="text", text=asm_txt) for asm_txt in contents]
@@ -115,13 +81,10 @@ async def serve() -> None:
             #     name="get_resource_url_related",  # type: str, the function name of this tool
             #     # actually it just is a display name if you already implemented the call_tool method
             #     description="Provide all possible relevant resource url based on the panda assembly code. The assembly code snippets provided should be as complete as possible.",
-            #     # the most important field for LLM to understand what this tool is doing
             #     inputSchema={
             #         "type": "object",
             #         "properties": {"panda_assembly_code": {"type": "string", "description": "The given panda assembly code snippets"}},
-            #         "required": ["panda_assembly_code"],
             #     },
-            # dict[str, Any] # set input schema using pydantic model
             Tool(
                 name="get_resource_related",  # type: str, the function name of this tool
                 # actually it just is a display name if you already implemented the call_tool method
@@ -131,23 +94,20 @@ async def serve() -> None:
                     "type": "object",
                     "properties": {"code_or_name": {"type": "string", "description": "Panda assembly code snippets or module method name."}},
                     "required": ["code_or_name"],
-                },
+                },  # dict[str, Any] # set input schema using pydantic model
             )
         ]
 
     @server.call_tool()
-    async def call_tool(name: str, arguments: dict, ctx: RequestContext) -> list[TextContent]:
+    async def call_tool(name: str, arguments: dict) -> list[TextContent]:
         """reponse of Client->Server tools/call request"""
         Log.info(f"tools/call called")
-        Log.info(f"ctx.request_id is {ctx.request_id}")
         match name:
             # case "get_resource_url_related":  # should match the str-id in tools/list
             #     panda_assembly_code = arguments.get("panda_assembly_code")
             #     if not panda_assembly_code:
             #         raise ValueError("Missing required argument: panda_assembly_code")
-
-            #     result = r"panda://%26vulwebview.src.main.ets.pages.Index%26.%23~%400%3E%23aboutToAppear"
-            #     return [TextContent(type="text", text=f"{result}")]
+            #     return [TextContent(type="text", text=r"panda://%26vulwebview.src.main.ets.pages.Index%26.%23~%400%3E%23aboutToAppear")]
             case "get_resource_related":
                 code_or_name = arguments.get("code_or_name")
                 if not code_or_name:
@@ -160,15 +120,14 @@ async def serve() -> None:
 
     @server.list_resources()
     async def list_all_resources() -> list[types.Resource]:
-        module_methd_name_l = arkts_api.get_all_module_method()
+        module_methd_name_l = await arkts_api.get_all_module_method()
         Log.info(f"resource: list. module_methd_name_l len = {len(module_methd_name_l)}")
         pa_resources = [
             types.Resource(
                 uri=AnyUrl(f"panda://{quote(module_method_name)}"),  # quote(module_method_name)
                 name=module_method_name,
                 title="Panda Assembly of " + module_method_name,
-                description="The decompiled assembly code with Panda Assembly format of module.method = "
-                + module_method_name,
+                description=f"The ArkTS decompiled assembly code with Panda Assembly format of {module_method_name}",
                 mimeType="text/plain",
             )
             for module_method_name in module_methd_name_l
@@ -177,7 +136,7 @@ async def serve() -> None:
 
     @server.read_resource()
     async def read_resource_impl(uri: AnyUrl) -> list[ReadResourceContents]:
-        contents = await read_pa_by_url(uri)
+        contents = await arkts_api.read_pa_by_url(uri)
         return [ReadResourceContents(content=asm_txt, mime_type="text/plain") for asm_txt in contents]
 
     @server.list_prompts()
